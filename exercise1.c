12d66626d12cf220c7f20dfd91dcd8fe707818d2
shivin88 <shivingoyal@yahoo.com>
2024-05-22 22:05:14 +0530

    program to create table without using loops

---
+++ exercise1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int num;
+    printf("please nter any no.=");
+    scanf("%d",&num);
+        printf("%d*1=%d\n",num,num*1);
+        printf("%d*2=%d\n",num,num*2);
+        printf("%d*3=%d\n",num,num*3);
+        printf("%d*4=%d\n",num,num*4);
+        printf("%d*5=%d\n",num,num*5);
+        printf("%d*6=%d\n",num,num*6);
+        return 0 ;
+}
